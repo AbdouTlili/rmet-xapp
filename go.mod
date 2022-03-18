bc336016d49cbebbfc965d2124b34de8ef3ff9a5
AbdouTlili <ha_tlili@esi.dz>
2022-03-18 10:49:42 +0100

    initial southbound code manager

--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
 module github.com/AbdouTlili/met-xapp
 
 go 1.16
-replace github.com/AbdouTlili/rmet-xapp => /home/zannnt/go/src/github.com/AbdouTlili/rmet-xapp
 
+replace github.com/AbdouTlili/rmet-xapp => /home/zannnt/go/src/github.com/AbdouTlili/rmet-xapp
 
 require (
 	github.com/AbdouTlili/rmet-xapp v0.0.0-20220315175810-7e6402eb682a
+	github.com/onosproject/onos-api/go v0.9.8
+	github.com/onosproject/onos-kpimon v0.3.5
 	github.com/onosproject/onos-lib-go v0.8.13
+	github.com/onosproject/onos-ric-sdk-go v0.8.7
 )
--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -1,6 +1,10 @@
 package manager
 
 import (
+	appConfig "github.com/AbdouTlili/rmet-xapp/pkg/config"
+	"github.com/AbdouTlili/rmet-xapp/pkg/southbound/e2/subscription"
+
+	// "github.com/AbdouTlili/rmet-xapp/pkg/store/measurements"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
 )
 
@@ -19,25 +23,73 @@ type Config struct {
 	SMVersion   string
 }
 
+// NewManager generates the new KPIMON xAPP manager
 func NewManager(config Config) *Manager {
-	//fmt.Printf("%#v", config)
-	log.Infof("%#v", config)
-	return &Manager{
-		appConfig:        config,
-		config:           config,
-		measurementStore: "string",
-		subManager:       "store",
+	appCfg, err := appConfig.NewConfig(config.ConfigPath)
+	if err != nil {
+		log.Warn(err)
 	}
-}
+	// subscriptionBroker := broker.NewBroker()
+	// measStore := measurements.NewStore()
+	// actionsStore := actions.NewStore()
 
-func (m Manager) Run() error {
-	return nil
+	subManager, err := subscription.NewManager(
+		subscription.WithE2TAddress("onos-e2t", 5150),
+		subscription.WithServiceModel(subscription.ServiceModelName(config.SMName),
+			subscription.ServiceModelVersion(config.SMVersion)),
+		subscription.WithAppConfig(appCfg),
+		subscription.WithAppID("onos-kpimon"),
+		// subscription.WithBroker(subscriptionBroker),
+		// subscription.WithActionStore(actionsStore),
+		// subscription.WithMeasurementStore(measStore)
+	)
+
+	if err != nil {
+		log.Warn(err)
+	}
+
+	manager := &Manager{
+		appConfig:  appCfg,
+		config:     config,
+		subManager: subManager,
+		// measurementStore: measStore,
+	}
+	return manager
 }
 
 // Manager is an abstract struct for manager
 type Manager struct {
-	appConfig        Config
-	config           Config
-	measurementStore string
-	subManager       string
+	appConfig appConfig.Config
+	config    Config
+	// measurementStore measurements.Store
+	subManager subscription.Manager
+}
+
+// Run runs KPIMON manager
+func (m *Manager) Run() {
+	err := m.start()
+	if err != nil {
+		log.Errorf("Error when starting KPIMON: %v", err)
+	}
+}
+
+// Close closes manager
+func (m *Manager) Close() {
+	log.Info("closing Manager")
+}
+
+func (m *Manager) start() error {
+	// err := m.startNorthboundServer()
+	// if err != nil {
+	// 	log.Warn(err)
+	// 	return err
+	// }
+
+	err := m.subManager.Start()
+	if err != nil {
+		log.Warn(err)
+		return err
+	}
+
+	return nil
 }
